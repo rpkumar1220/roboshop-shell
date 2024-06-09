6c12aed4e2af5df98b8cc7c06a74339f8bf41eb9
rpraveenkumar1220 <rpraveenkumar1220@gmail.com>
2024-06-09 21:54:44 +0530

    Setting up roboshop project with shell scripting

--- shipping/shipping.sh
+++ shipping/shipping.sh
@@ -30,7 +30,7 @@ echo "Installing mysql client"
 dnf install mysql -y
 
 echo "loading schema"
-mysql -h MYSQL-SERVER-IPADDRESS -uroot -pRoboShop@1 < /app/schema/shipping.sql
+mysql -h 172.31.19.254 -uroot -pRoboShop@1 < /app/schema/shipping.sql
 
 echo "restarting shipping service"
 systemctl restart shipping
