6a69ffd70423964688762a2f2e3d28504d1f0ff5
rpkumar1220 <rpkumardba@gmail.com>
2024-06-10 21:15:49 +0530

    Update catalogue.sh

--- catalogue/catalogue.sh
+++ catalogue/catalogue.sh
@@ -1,35 +1,13 @@
-echo "Setting up NodeJS Repo"
-dnf module disable nodejs -y && dnf module enable nodejs:18 -y
+source common.sh 
+nodejs
+mongodb_setup
 
-echo "Installing NodeJS RPM"
-dnf install nodejs -y
 
-echo "Adding APP user"
-useradd roboshop
 
-echo "Creating APP directory"
-mkdir /app
 
-echo "Downloading catalogue content and extracting it"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip && cd /app && unzip /tmp/catalogue.zip
 
-echo "Downloading NodeJS Dependencies"
-cd /app && npm install
 
-echo "Copying catalogue service file"
-cp /home/centos/roboshop-shell/catalogue/catalogue.service /etc/systemd/system/catalogue.service
 
-echo "Reloading Daemon"
-systemctl daemon-reload
 
-echo "Enabling and restarting catalogue service"
-systemctl enable catalogue && systemctl start catalogue
 
-echo "setting up mongodb repo"
-cp /home/centos/roboshop-shell/mongodb/mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo "Installing mongodb client"
-dnf install mongodb-org-shell -y
-
-echo "Loading the schema"
-mongo --host 172.31.21.133 </app/schema/catalogue.js
